296bf32abfb81455b3e4c8956323046f56bf2571
Steve Bosman <stevebosman@gmail.com>
2023-03-12 23:06:50 +0000

    change project name
    Signed-off-by: Steve Bosman <stevebosman@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/stevebosman/is-close-kt")
+            url = uri("https://maven.pkg.github.com/stevebosman/difference")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
